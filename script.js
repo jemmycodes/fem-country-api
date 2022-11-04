c9d69d2a7bb32f703293db0c1841c99f2c040a1e
jemmy <flashyjotebele@gmail.com>
2022-11-04 11:59:01 -1100

    search feature implemented

--- script.js
+++ script.js
@@ -2,33 +2,34 @@
 
 const container = document.getElementById("country-container");
 const select = document.getElementById("select-btn");
+const search = document.getElementById("search-box");
 
 const getApi = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
-  renderCountry(data);
+  data.forEach((country) => {
+    renderCountry(country);
+  });
   return data;
 };
 
 const renderCountry = (data) => {
-  data.forEach((country) => {
-    const html = `<figure class="w-[15rem]">
-        <img src="${country.flags.png}" alt="" class="rounded-t-md w-full h-32" />
+  const html = `<figure class="w-[15rem]">
+        <img src="${data.flags.png}" alt="" class="rounded-t-md w-full h-32" />
         <hgroup class="bg-DarkModeTextLightModeElements rounded-b-md p-5">
-          <p class="font-extrabold pb-4">${country.name.common}</p>
+          <p class="font-extrabold pb-4">${data.name.common}</p>
           <p>
-            Population: <span>${country.population}</span>
+            Population: <span>${data.population}</span>
           </p>
           <p>
-            Region: <span>${country.region}</span>
+            Region: <span>${data.region}</span>
           </p>
           <p>
-            Capital: <span>${country.capital[0]}</span>
+            Capital: <span>${data.capital[0]}</span>
           </p>
         </hgroup>
       </figure>`;
-    container.insertAdjacentHTML("beforeend", html);
-  });
+  container.insertAdjacentHTML("beforeend", html);
 };
 
 const chooseRegion = async () => {
@@ -37,9 +38,27 @@ const chooseRegion = async () => {
     `https://restcountries.com/v3.1/region/${select.value}`
   );
   const data = await response.json();
-  console.log(data);
-  renderCountry(data);
+  data.forEach((country) => {
+    renderCountry(country);
+  });
 };
+
+const renderSearch = async () => {
+  container.replaceChildren();
+  const response = await fetch(
+    `https://restcountries.com/v3.1/name/${search.value}?fullText=true`
+  );
+  const data = await response.json();
+  const [country] = data;
+
+  renderCountry(country);
+};
+
+search.addEventListener("keydown", function (e) {
+  if (e.code === "Enter") {
+    renderSearch();
+  }
+});
 
 select.addEventListener("change", chooseRegion);
 
